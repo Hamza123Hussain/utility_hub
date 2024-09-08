21b027485ae01bc54e6bec1f016d8c5290f3d8df
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-08 05:11:36 +0500

    routes added

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import { Route, Routes } from 'react-router-dom'
 import ColorPicker from './components/Tools/ColorPicker'
+import UnitConverter from './components/Tools/Unitconverter/UnitConverter'
+
 const App: React.FC = () => {
   return (
-    <div className=" flex flex-col gap-2 ">
+    <div className=" flex flex-col bg-[#333333] min-h-screen  ">
       <Navbar />
-      <div className=" flex-1">
+      <div className=" flex-1 my-10">
         <Routes>
-          <Route path="/" element={<ColorPicker />} />
+          <Route path="/ColorPicker" element={<ColorPicker />} />
+          <Route path="/UnitConverter" element={<UnitConverter />} />
         </Routes>
       </div>
       <Footer />
